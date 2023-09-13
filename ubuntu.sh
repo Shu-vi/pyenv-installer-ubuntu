6a63ddbfe440e4eebe3d53a23c2cb21ad1d38a78
Vadim <vad.general@mail.ru>
2023-09-13 18:37:05 +0300

    Fix: исправить опечатку в названии библиотеки

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+cp ubuntu.sh ../
+cd ~
+rm -rf Artificial-intelligence-systems
 mkdir python
 cd python
 sudo apt-get update
@@ -7,7 +9,7 @@ sudo apt-get upgrade
 git clone https://github.com/Shu-vi/Artificial-intelligence-systems.git
 curl https://pyenv.run | bash
 sudo apt update
-sudo apt install -y build-essential libssl-dev zliblg-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev
+sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev
 exec $SHELL
 echo 'export PYENV_ROOT ="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
