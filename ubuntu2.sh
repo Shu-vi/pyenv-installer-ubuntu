dc76b43c4f9b72e78b4d439d9c6d5623ab4406e3
Vadim <vad.general@mail.ru>
2023-09-14 15:15:19 +0300

    Feat: разделить скрипт на два

---
+++ ubuntu2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd Artificial-intelligence-systems
+pyenv install 3.7.10
+pyenv local 3.7.10
+python -m venv env
+source env/bin/activate
+pip install --upgrade pip
+pip install deeppavlov
+python -m deeppavlov install squad_bert
+pip install pyTelegramBotAPI
+python main.py
